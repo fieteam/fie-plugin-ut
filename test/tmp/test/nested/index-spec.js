c7d33889dba2d73767b190b3974b52a375f59d64
zincli <fly-lx@163.com>
2016-12-06 13:30:42 +0800

    删除测试用临时目录，尝试修复appver的测试

--- test/tmp/test/nested/index-spec.js
+++
@@ -1,5 +0,0 @@
-describe('nested', function (){
-  it('这个测试应该运行', function (){
-
-  })
-})
--- test/tmp/test/setup.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-const sinon = require('sinon');
-const request = require('supertest');
-
-global.should = chai.should();
-global.expect = chai.expect;
-global.sinon = sinon;
-global.request = request;
-
